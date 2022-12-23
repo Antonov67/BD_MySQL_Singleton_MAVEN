ccda6085819c2052e03b6a743000445bdea50bb9
Dm Ant <Dm@Ant.ru>
2022-12-23 13:52:09 +0300

    Initial commit

---
+++ src/main/java/org/example/BDSettings.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class BDSettings {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/java1";
+    public static final String DB_USER = "root";
+    public static final String DB_PSWRD = "root";
+
+}
